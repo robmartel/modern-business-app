253f6a40eb23f00b54831594b5b38192cdbead1f
robmartel <99683835+robmartel@users.noreply.github.com>
2023-03-08 14:37:53 -0500

    created the clients component and dynamical rendered them by using the map function, they are responsive

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -10,7 +10,7 @@ const CTA = () =>
       </div>
 
       <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
-        <Button />
+        <Button /> 
       </div>
     </section>
   )
